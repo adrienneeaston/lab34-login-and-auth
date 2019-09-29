1e44e71cc638ee1843c86cb1b085c48e82234bf5
adrienneeaston <39181121+adrienneeaston@users.noreply.github.com>
2019-09-28 18:42:45 -0700

    close to working

--- src/app.js
+++ src/app.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React from "react";
+import LoginContext from "./components/auth/context.js";
+import Login from "./components/auth/login.js";
+import Auth from "./components/auth/auth.js";
+import ToDo from "./components/todo/todo.js";
 
-import ToDo from './components/todo/todo.js';
+const App = props => {
+  return (
+    <LoginContext>
+      <header>
+        <Login />
+      </header>
+      <section>
+        <Auth>
+          <div>Welcome Home</div>
+          <ToDo />
+        </Auth>
+        <Auth capability="editor">
+          <button>Write an Article</button>
+        </Auth>
+      </section>
+    </LoginContext>
+  );
+};
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <>
-        <ToDo />
-      </>
-    );
-  }
-}
+// import React from 'react';
+
+// import ToDo from './components/todo/todo.js';
+
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <>
+//         <ToDo />
+//       </>
+//     );
+//   }
+// }
--- src/components/auth/context.js
+++ src/components/auth/context.js
@@ -1,59 +1,53 @@
-import React from 'react';
-import cookie from 'react-cookies';
-import jwt from 'jsonwebtoken';
+import React from "react";
+// import cookie from "react-cookies";
+import jwt from "jsonwebtoken";
 
 const API = process.env.REACT_APP_API;
 
-const testLogins = {
-  testAdmin: process.env.REACT_APP_ADMIN_TOKEN || '',
-  testEditor: process.env.REACT_APP_EDITOR_TOKEN || '',
-  testUser: process.env.REACT_APP_USER_TOKEN || '',
-};
-
 export const LoginContext = React.createContext();
 
-class LoginProvider extends React.Component {
+const testLogins = {
+  testAdmin: process.env.REACT_APP_ADMIN_TOKEN || "",
+  testEditor: process.env.REACT_APP_EDITOR_TOKEN || "",
+  testUser: process.env.REACT_APP_USER_TOKEN || ""
+};
+
+class LoginContextProvider extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       loggedIn: false,
       login: this.login,
       logout: this.logout,
-      user: {},
+      user: {}
     };
   }
 
-  login = (username, password) => {
-    // This is foul and unsafe ... but when working offline / testmode ess oh kay
-    if (testLogins[username]) {
-      this.validateToken(testLogins[username]);
-    }
-    else {
-      fetch(`${API}/signin`, {
-        method: 'post',
-        mode: 'cors',
-        cache: 'no-cache',
-        headers: new Headers({
-          "Authorization": `Basic ${btoa(`${username}:${password}`)}`
-        })
+  login = user => {
+    const auth = `Basic ${btoa(`${user.username}:${user.password}`)}`;
+
+    fetch(`${API}/signin`, {
+      method: "post",
+      mode: "cors",
+      cache: "no-cache",
+      headers: new Headers({
+        Authorization: `${auth}`
       })
-        .then(response => response.text())
-        .then(token => this.validateToken(token))
-        .catch(console.error);
-    }
-  }
+    })
+      .then(response => response.text())
+      .then(token => this.validateToken(token))
+      .catch(console.error);
+  };
 
   validateToken = token => {
     try {
-      let user = jwt.verify(token, process.env.REACT_APP_SECRET)
-      console.log('all good');
+      let user = jwt.verify(token, process.env.REACT_APP_SECRET);
+      console.log("all good");
       this.setLoginState(true, token, user);
-    }
-    catch (e) {
+    } catch (e) {
       this.setLoginState(false, null, {});
       console.log("Token Validation Error", e);
     }
-
   };
 
   logout = () => {
@@ -61,7 +55,7 @@ class LoginProvider extends React.Component {
   };
 
   setLoginState = (loggedIn, token, user) => {
-    cookie.save('auth', token);
+    cookie.save("auth", token);
     this.setState({ token, loggedIn, user });
   };
 
@@ -74,4 +68,82 @@ class LoginProvider extends React.Component {
   }
 }
 
-export default LoginProvider;
+export default LoginContextProvider;
+
+// import React from 'react';
+// import cookie from 'react-cookies';
+// import jwt from 'jsonwebtoken';
+
+// const API = process.env.REACT_APP_API;
+
+// const testLogins = {
+//   testAdmin: process.env.REACT_APP_ADMIN_TOKEN || '',
+//   testEditor: process.env.REACT_APP_EDITOR_TOKEN || '',
+//   testUser: process.env.REACT_APP_USER_TOKEN || '',
+// };
+
+// export const LoginContext = React.createContext();
+
+// class LoginProvider extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       loggedIn: false,
+//       login: this.login,
+//       logout: this.logout,
+//       user: {},
+//     };
+//   }
+
+//   login = (username, password) => {
+//     // This is foul and unsafe ... but when working offline / testmode ess oh kay
+//     if (testLogins[username]) {
+//       this.validateToken(testLogins[username]);
+//     }
+//     else {
+//       fetch(`${API}/signin`, {
+//         method: 'post',
+//         mode: 'cors',
+//         cache: 'no-cache',
+//         headers: new Headers({
+//           "Authorization": `Basic ${btoa(`${username}:${password}`)}`
+//         })
+//       })
+//         .then(response => response.text())
+//         .then(token => this.validateToken(token))
+//         .catch(console.error);
+//     }
+//   }
+
+//   validateToken = token => {
+//     try {
+//       let user = jwt.verify(token, process.env.REACT_APP_SECRET)
+//       console.log('all good');
+//       this.setLoginState(true, token, user);
+//     }
+//     catch (e) {
+//       this.setLoginState(false, null, {});
+//       console.log("Token Validation Error", e);
+//     }
+
+//   };
+
+//   logout = () => {
+//     this.setLoginState(false, null, {});
+//   };
+
+//   setLoginState = (loggedIn, token, user) => {
+//     cookie.save('auth', token);
+//     this.setState({ token, loggedIn, user });
+//   };
+
+//   render() {
+//     return (
+//       <LoginContext.Provider value={this.state}>
+//         {this.props.children}
+//       </LoginContext.Provider>
+//     );
+//   }
+// }
+
+// export default LoginProvider;
